3c3cb1819b4915971c557b39688b0f7985c1b9f6
sdifrancisco28 <santidifrancisco@gmail.com>
2024-06-04 19:28:29 -0300

    Se agregó la parte de newrelic para monitorear la aplicación

--- newrelic.js
+++ newrelic.js
@@ -1,6 +1,6 @@
 exports.config = {
     app_name: ['node-api-devops'],
-    license_key: '64a89c6665a002a58f893f50e9224171FFFFNRAL',
+    license_key: '33f7465e848a2010d32646c0178c190eFFFFNRAL',
     logging: {
       level: 'info',
     },
