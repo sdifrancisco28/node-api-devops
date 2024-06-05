a526063ace03fc28de0d5df88f73acef9a3fe635
sdifrancisco28 <santidifrancisco@gmail.com>
2024-06-05 18:14:59 -0300

    Modificamos console log

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 })
 
 const server = app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+  console.log('My Server is running on port 3000!')
 })
 
 module.exports = { app, server }
